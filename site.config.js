e3bdfe078789f1b23652ff1b251b886698dda591
Luckydipper <qhrqufdlek@naver.com>
2021-07-25 02:24:46 +0900

    [add] - site theme
    I think it better to use notion application. why shoude be web?

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '/981ae3bb4ef74520a20c01524b6c2757',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
